4ed2141f926110a34f23458411b933583581ffa0
Farah Diouani <farah.diouani@esprit.tn>
2023-02-18 22:07:16 +0100

    Update
    update

---
+++ src/Controller/TournoisController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TournoisController extends AbstractController
+{
+    #[Route('/game', name: 'app_tournois')]
+    public function index(): Response
+    {
+        return $this->render('tournois/index.html.twig', [
+            'controller_name' => 'TournoisController',
+        ]);
+    }
+}
