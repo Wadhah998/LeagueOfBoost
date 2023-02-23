307543639dd79a1e7a32b98ec527a0c7c5befeae
“[MbarkiAchraf]” <[mohamedachraf.mbarki@esprit.tn]>
2023-02-23 16:01:53 +0100

    boosting

--- migrations/Version20230222163037.php
+++ migrations/Version20230222163037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230221141334 extends AbstractMigration
+final class Version20230222163037 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230221141334 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE team ADD name VARCHAR(255) NOT NULL, ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE booster ADD availability TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE team DROP name, DROP description');
+        $this->addSql('ALTER TABLE booster DROP availability');
     }
 }
---
+++ src/Form/ReservationBType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ReservationB;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+class ReservationBType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('oldrank')
+            ->add('newrank')
+            ->add('newprice')
+            ->add('booster', HiddenType::class, [
+                'data' => $options['booster_id'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ReservationB::class,
+            'booster_id' => null,
+        ]);
+    }
+}
+
