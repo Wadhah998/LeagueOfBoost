7ff47970f730fcff4f335c1211e000a4055be928
Wadhah998 <daoidwadhah@gmail.com>
2023-02-27 17:38:40 +0100

    adminDash

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,9 +25,11 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
 
+    public function logout(Twig\Environment $twig): Response
+    {
+        $content = $twig->render('security/logout.html.twig');
+
+        return new Response($content);
     }
 }
