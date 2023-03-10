e8914ce15176d1183088570e8e4df94ccf00b275
wassimghamgui <mohamedwassim.ghamgui@esprit.tn>
2023-03-10 11:07:45 +0100

    kol chay nrml2

---
+++ migrations/Version20230310100726.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230310100726 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rating DROP INDEX UNIQ_D88926222D6BA2D9, ADD INDEX IDX_D88926222D6BA2D9 (reclamation_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rating DROP INDEX IDX_D88926222D6BA2D9, ADD UNIQUE INDEX UNIQ_D88926222D6BA2D9 (reclamation_id)');
+    }
+}
