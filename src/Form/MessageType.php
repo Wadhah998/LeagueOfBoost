8cb696c9825e3335b069962e6e280ad1e85f0802
Farah Diouani <farah.diouani@esprit.tn>
2023-05-10 01:52:28 +0100

    wassim
    wassim

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -6,15 +6,21 @@ use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message')
-        ;
-    }
+        ->add('message', TextType::class, [
+            'label' => 'Type your message  :',
+            'attr' => ['class' => 'message-input'],
+
+        ])
+    ;
+}
 
     public function configureOptions(OptionsResolver $resolver): void
     {
